8631d70d9fad50332980671ee59d395fc744f4ef
pyoor <pyoor@users.noreply.github.com>
2025-09-22 10:23:55 -0400

    refactor: apply fixes pre-commit for pre-commit errors

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,14 @@
 try:
     from urllib import parse
-except:
+except ImportError:
     import urllib as parse
 from bugsy import Bugsy
 
 
 def rest_url(*parts, **kwargs):
-    base = '/'.join(['https://bugzilla.mozilla.org/rest'] +
-                    [str(p) for p in parts])
-    kwargs.setdefault('include_fields', Bugsy.DEFAULT_SEARCH)
+    base = "/".join(["https://bugzilla.mozilla.org/rest"] + [str(p) for p in parts])
+    kwargs.setdefault("include_fields", Bugsy.DEFAULT_SEARCH)
     params = parse.urlencode(kwargs, True)
     if params:
-        return '%s?%s' % (base, params)
+        return "%s?%s" % (base, params)
     return base
